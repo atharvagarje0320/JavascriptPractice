d6f2e9af1302ddf90016edd67db787084bc27d32
atharvagarje0320 <110807121+atharvagarje0320@users.noreply.github.com>
2023-08-10 18:48:39 +0000

    First Day Done

---
+++ Javascript/script.js
@@ -0,0 +1,39 @@
+// function LoginUser(username ){ 
+//     if(!username)
+//     {
+//         console.log("Please enter the username ")
+//         return
+//     } 
+
+//     return`${username} just logged in`
+
+// }
+// console.log(LoginUser())
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+
+// function AddObject(...num1){
+//     return num1
+// }
+// console.log(AddObject(11 ,12, 14));
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+// const user={
+//     username: "Atharva",
+//     Age  : 19
+// }
+// function AddNamePrice(){
+//     console.log(`Here UserName is ${user.username} and age is ${user.Age}`);
+// }
+
+// AddNamePrice()
+
+// --------------------------------------------------------------------------------------------------------------------------
+// function AddNamePrice(user){
+//     console.log(`Here UserName is ${user.username} and age is ${user.Age}`);
+// }
+
+// AddNamePrice({
+//     username:"Atharva",
+//     Age:19
+// })
