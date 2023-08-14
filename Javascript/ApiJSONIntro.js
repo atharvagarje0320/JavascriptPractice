c740326c56e01f7170c74d52ea4c5e4f423b77de
atharvagarje0320 <110807121+atharvagarje0320@users.noreply.github.com>
2023-08-14 15:05:05 +0000

    API &  JSON

---
+++ Javascript/ApiJSONIntro.js
@@ -0,0 +1,32 @@
+const MySelf = { 
+    name : "Atharva",
+    rollno : 20,
+    Address :'Pune ',
+    designation : "Student"
+}
+
+console.log(MySelf["name"]);
+
+//Destructring of Object 
+const {name:FullName} = MySelf
+console.log(FullName);
+
+
+
+const {rollno : no } = MySelf
+console.log(no);
+ 
+// API --- JSON FORMAT
+{
+    
+}//object
+
+
+[
+    {},
+    {}
+]// IN ARRAY FORMAT CONSISTING OBJECTS .
+
+
+
+
